d295445729d3a7e499462b4957655472f1c970db
dnzetta <darlenenabilazetta.d#@kkumail.com>
2026-03-18 11:18:10 +0700

    update

--- web.py
+++ web.py
@@ -417,7 +417,7 @@ with tab1:
     if "last_result" in st.session_state:
         prob, stack, graph, consensus = st.session_state["last_result"]
 
-        st.markdown("#### Agreement Prediction")
+        st.markdown("##### Agreement Prediction")
         st.markdown(f"### {consensus}")
 
         st.write("**Base Model Outputs**")
